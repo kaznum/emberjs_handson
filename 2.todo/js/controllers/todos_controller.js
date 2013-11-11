ee64c65c86823e4d03dea09ed74b40044730f3eb
Kazuya NUMATA <numata@gmail.com>
2013-11-11 14:58:48 +0900

    Add some comments

--- 2.todo/js/controllers/todos_controller.js
+++ 2.todo/js/controllers/todos_controller.js
@@ -21,6 +21,10 @@ Todos.TodosController = Ember.ArrayController.extend({
   remaining: function () {
     return this.filterBy('isCompleted', false).get('length');
   }.property('@each.isCompleted'),
+  //@each: 各要素のisCompletedが変更になった時、remainingが再計算されることを明示(ArrayControllerやArrayに類する場合(enumerablesの場合)に@eachが使用できる)
+  // ArrayすらEmberjsを読み込むとprototypeレベルで拡張される。
+  // See http://emberjs.com/guides/enumerables/
+  //     http://emberjs.com/api/classes/Ember.Enumerable.html
 
   inflection: function () {
     var remaining = this.get('remaining');
