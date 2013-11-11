16ef39dfde9b885c030cc086736e42df6031763c
Kazuya NUMATA <numata@gmail.com>
2013-11-11 11:54:56 +0900

    Add comments

--- 2.todo/js/router.js
+++ 2.todo/js/router.js
@@ -1,9 +1,16 @@
+// Router はどのテンプレートを使うか決める（という説明はあまり正しくないけど)
+// ここでは、todos というテンプレートが使われるようになる
+// どのモデルを使うかも決める
 Todos.Router.map(function () {
   this.resource('todos', { path: '/'} );
 });
 
+// どんな振る舞いをするかを決める
+// model のfunctionの戻り値がRouterで定義されたtodosテンプレートに返される
+// templateの#each の引数（省略されている)に渡される( {{#each this}} )
 Todos.TodosRoute = Ember.Route.extend({
   model: function () {
+    // todo リストの全レコードを返す
     return this.store.find('todo');
   }
 });
