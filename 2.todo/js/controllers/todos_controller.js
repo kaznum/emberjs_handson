80b348bfa03b3e96cb43124ced5a38e0838a37ea
Kazuya NUMATA <numata@gmail.com>
2013-11-12 09:59:09 +0900

    Implement 'allAreDone'

--- 2.todo/js/controllers/todos_controller.js
+++ 2.todo/js/controllers/todos_controller.js
@@ -43,5 +43,9 @@ Todos.TodosController = Ember.ArrayController.extend({
 
   completed: function () {
     return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted'),
+
+  allAreDone: function (key, value) {
+    return !!this.get('length') && this.everyBy('isCompleted', true);
   }.property('@each.isCompleted')
 })
