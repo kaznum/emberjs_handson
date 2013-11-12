471cb632596241c4ee301f9f4df77eb97abc4eb1
Kazuya NUMATA <numata@gmail.com>
2013-11-12 09:39:53 +0900

    Implement 'clearCompleted'

--- 2.todo/js/controllers/todos_controller.js
+++ 2.todo/js/controllers/todos_controller.js
@@ -15,6 +15,12 @@ Todos.TodosController = Ember.ArrayController.extend({
       this.set('newTitle', '');
 
       todo.save();
+    },
+
+    clearCompleted: function () {
+      var completed = this.filterBy('isCompleted', true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
     }
   },
 
@@ -29,5 +35,13 @@ Todos.TodosController = Ember.ArrayController.extend({
   inflection: function () {
     var remaining = this.get('remaining');
     return remaining === 1 ? 'item' : 'items';
-  }.property('remaining')
+  }.property('remaining'),
+
+  hasCompleted: function () {
+    return this.get('completed') > 0;
+  }.property('completed'),
+
+  completed: function () {
+    return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted')
 })
